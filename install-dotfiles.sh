73ef76463546654586550f5259106d451b01d7b0
Dimitris Trechas <dimitristrechas@gmail.com>
2025-12-06 14:02:39 +0200

    feat: add helper text after stow

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -6,3 +6,5 @@ cd ~/dev/dotfiles
 stow -t ~ --adopt ghostty
 stow -t ~ --adopt starship
 stow -t ~ --adopt git
+
+echo "You might want to cd into dotfiles directory and 'git checkout .' to undo changes in the configs"
